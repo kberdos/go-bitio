da61b5dd510d31a4bd8720f1fe5ad527065f3f7d
Kazuya Erdos <kazuya_erdos@brown.edu>
2025-01-18 14:49:19 -0500

    read tests initially passing

--- read_test.go
+++ read_test.go
@@ -1,7 +1,6 @@
 package bitio_test
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
@@ -9,10 +8,58 @@ import (
 )
 
 func TestRead(t *testing.T) {
-	t.Run("read small", testReadSmall)
+	t.Run("read bit", testReadBit)
+	t.Run("read bits", testReadBits)
+	t.Run("read byte", testReadByte)
 }
 
-func testReadSmall(t *testing.T) {
+func testReadBits(t *testing.T) {
+	file, err := os.Open("./testfiles/readtest")
+	if err != nil {
+		t.Fatal("error opening file", err)
+	}
+	bw, err := bitio.NewReader(file)
+	if err != nil {
+		t.Fatal("error creating reader", err)
+	}
+	// 'H' = 01001000
+	x, err := bw.ReadBits(2) // 01
+	if err != nil {
+		t.Fatal("error reading from file", err)
+	}
+	if x != 1 {
+		t.Fatalf("incorrect value read, received %d\n", x)
+	}
+
+	x, err = bw.ReadBits(4) // 0010
+	if err != nil {
+		t.Fatal("error reading from file", err)
+	}
+	if x != 2 {
+		t.Fatalf("incorrect value read, received %d\n", x)
+	}
+}
+
+func testReadBit(t *testing.T) {
+	file, err := os.Open("./testfiles/readtest")
+	if err != nil {
+		t.Fatal("error opening file", err)
+	}
+	bw, err := bitio.NewReader(file)
+	if err != nil {
+		t.Fatal("error creating reader", err)
+	}
+	// 'H' = 01001000
+	x, err := bw.ReadBits(1) // 0
+	if err != nil {
+		t.Fatal("error reading from file", err)
+	}
+	if x != 0 {
+		t.Fatalf("incorrect value read, received %d\n", x)
+	}
+}
+
+func testReadByte(t *testing.T) {
 	file, err := os.Open("./testfiles/readtest")
 	if err != nil {
 		t.Fatal("error opening file", err)
@@ -25,5 +72,7 @@ func testReadSmall(t *testing.T) {
 	if err != nil {
 		t.Fatal("error reading from file", err)
 	}
-	fmt.Println(x)
+	if x != 72 {
+		t.Fatalf("incorrect value read, received %d\n", x)
+	}
 }
