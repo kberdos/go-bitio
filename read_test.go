0dfe1e034f265f03cad4d1f69668bc45781cfe15
Kazuya Erdos <kazuya_erdos@brown.edu>
2025-01-18 14:56:55 -0500

    v0.1.3

--- read_test.go
+++ read_test.go
@@ -11,6 +11,27 @@ func TestRead(t *testing.T) {
 	t.Run("read bit", testReadBit)
 	t.Run("read bits", testReadBits)
 	t.Run("read byte", testReadByte)
+	t.Run("read bytes", testReadBytes)
+	// TODO: reading past the end errors
+}
+
+func testReadBit(t *testing.T) {
+	file, err := os.Open("./testfiles/readtest")
+	if err != nil {
+		t.Fatal("error opening file", err)
+	}
+	bw, err := bitio.NewReader(file)
+	if err != nil {
+		t.Fatal("error creating reader", err)
+	}
+	// 'H' = 01001000
+	x, err := bw.ReadBits(1) // 0
+	if err != nil {
+		t.Fatal("error reading from file", err)
+	}
+	if x != 0 {
+		t.Fatalf("incorrect value read, received %d\n", x)
+	}
 }
 
 func testReadBits(t *testing.T) {
@@ -40,25 +61,6 @@ func testReadBits(t *testing.T) {
 	}
 }
 
-func testReadBit(t *testing.T) {
-	file, err := os.Open("./testfiles/readtest")
-	if err != nil {
-		t.Fatal("error opening file", err)
-	}
-	bw, err := bitio.NewReader(file)
-	if err != nil {
-		t.Fatal("error creating reader", err)
-	}
-	// 'H' = 01001000
-	x, err := bw.ReadBits(1) // 0
-	if err != nil {
-		t.Fatal("error reading from file", err)
-	}
-	if x != 0 {
-		t.Fatalf("incorrect value read, received %d\n", x)
-	}
-}
-
 func testReadByte(t *testing.T) {
 	file, err := os.Open("./testfiles/readtest")
 	if err != nil {
@@ -73,6 +75,26 @@ func testReadByte(t *testing.T) {
 		t.Fatal("error reading from file", err)
 	}
 	if x != 72 {
+		t.Fatalf("incorrect value read, received %d\n", x)
+	}
+}
+
+func testReadBytes(t *testing.T) {
+	file, err := os.Open("./testfiles/readtest_big")
+	if err != nil {
+		t.Fatal("error opening file", err)
+	}
+	bw, err := bitio.NewReader(file)
+	if err != nil {
+		t.Fatal("error creating reader", err)
+	}
+	// 'HELLO' = 72 69 76 76 79
+	// = 0100 1000 0100 0101 0100 1100 0100 1100 0100 1111
+	x, err := bw.ReadBits(15) // 0100 1000 0100 010
+	if err != nil {
+		t.Fatal("error reading from file", err)
+	}
+	if x != 9250 {
 		t.Fatalf("incorrect value read, received %d\n", x)
 	}
 }
